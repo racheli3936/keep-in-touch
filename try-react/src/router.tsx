5df97abce6c4fac83c9c12ecfe5fec616037db80
racheli3936 <racheli3936@gmail.com>
2025-06-04 16:18:48 +0300

    main error

--- try-react/src/router.tsx
+++ try-react/src/router.tsx
@@ -24,7 +24,7 @@ const Router = createBrowserRouter([
     {
         path: '/',
         element: <AppLayout />,
-        errorElement: <>Main error</>,
+        errorElement: <>Main error 33</>,
         children: [
             {
                 path: 'login',
