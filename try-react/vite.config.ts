43247134c7955ecb357bfbc19b0c9c99015623a6
racheli3936 <racheli3936@gmail.com>
2025-06-04 16:42:52 +0300

    main error2

--- try-react/vite.config.ts
+++ try-react/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    build: {
+    chunkSizeWarningLimit: 2000 // קבע את המגבלה ל-2000 KB
+  }
 })
